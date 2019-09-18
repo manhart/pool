c725d3ef4745d87e83ebe35be3aacc0228af31b8
Alexander Manhart <alexander@manhart-it.de>
2019-09-18 16:46:04 +0200

    alertbox.open

--- javascripts/jquery.ajax.js
+++ javascripts/jquery.ajax.js
@@ -127,7 +127,10 @@ function RequestPOOL(module, method, params, async)
 					}
 					else {
 						// wenn die alert box verfuegbar ist, zeigen wir die Fehlermeldung darin an
-						if(typeof alert_box == 'function') {
+						if (typeof MODULE_FUNCTIONS.alertbox.open == 'function') {
+							MODULE_FUNCTIONS.alertbox.open('Error', message);
+						}
+						else if(typeof alert_box == 'function') {
 							alert_box(message);
 						}
 						else {
