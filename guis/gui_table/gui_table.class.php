432df331fb4414b815b936adf2c9d2ca52909f7e
Alexander Manhart <alexander@manhart-it.de>
2021-04-15 08:52:52 +0200

    GUI_Table reworked (new orientation: write js code)

--- guis/gui_table/gui_table.class.php
+++ guis/gui_table/gui_table.class.php
@@ -7,17 +7,129 @@
  * @author Alexander Manhart <alexander@manhart-it.de>
  */
 
-
-class GUI_Table extends GUI_Module
+class GUI_Table extends GUI_Module implements JsonConfig
 {
+    protected array $defaultOptions = [
+        'url' => [
+            'attribute' => 'data-url',
+            'type' => 'string',
+            'value' => null // undefined
+        ],
+        'classes' => [
+            'attribute' => 'data-classes',
+            'type' => 'string',
+            'value' => 'table table-bordered table-hover'
+        ],
+        'clickToSelect' => [
+            'attribute' => 'data-click-to-select',
+            'type' => 'boolean',
+            'value' => false
+        ],
+        'columns' => [
+            'attribute' => '',
+            'type' => 'array',
+            'value' => []
+        ],
+        'customSort' => [
+            'attribute' => 'data-custom-sort',
+            'type' => 'function',
+            'value' => null // undefined
+        ],
+        'pagination' => [
+            'attribute' => 'data-pagination',
+            'type' => 'boolean',
+            'value' => false
+        ],
+        'resizable' => [
+            'attribute' => 'data-resizable',
+            'type' => 'boolean',
+            'value' => false
+        ],
+        'search' => [
+            'attribute' => 'data-search',
+            'type' => 'boolean',
+            'value' => false
+        ],
+        'showColumns' => [
+            'attribute' => 'data-show-columns',
+            'type' => 'boolean',
+            'value' => false
+        ],
+        'showFullscreen' => [
+            'attribute' => 'data-show-fullscreen',
+            'type' => 'boolean',
+            'value' => false,
+        ],
+        'showFullscreen' => [
+            'attribute' => 'data-show-fullscreen',
+            'type' => 'boolean',
+            'value' => false,
+        ],
+        'showRefresh' => [
+            'attribute' => 'data-show-refresh',
+            'type' => 'boolean',
+            'value' => false,
+        ],
+        'showToggle' => [
+            'attribute' => 'data-show-toggle',
+            'type' => 'boolean',
+            'value' => false,
+        ],
+        'sortable' => [
+            'attribute' => 'data-sortable',
+            'type' => 'boolean',
+            'value' => false
+        ]
+    ];
+
+    protected array $defaultColumnOptions = [
+        'align' => [
+            'attribute' => 'data-align',
+            'type' => 'string',
+            'value' => null
+        ],
+        'field' => [
+            'attribute' => 'data-field',
+            'type' => 'string',
+            'value' => null
+        ],
+        'formatter' => [
+            'attribute' => 'data-formatter',
+            'type' => 'function',
+            'value' => null
+        ],
+        'sortable' => [
+            'attribute' => 'data-sortable',
+            'type' => 'boolean',
+            'value' => false
+        ],
+        'sorter' => [
+            'attribute' => 'data-sorter',
+            'type' => 'function',
+            'value' => null
+        ],
+        'title' => [
+            'attribute' => 'data-title',
+            'type' => 'string',
+            'value' => null
+        ],
+        'visible' => [
+            'attribute' => 'data-visible',
+            'type' => 'boolean',
+            'value' => true
+        ],
+    ];
+
+    protected array $options = [];
+
     /**
      * @var array options for the table
      */
-    protected array $options = [
-        'data-url' => null,
+    protected array $attribute = [
+  /*      'data-url' => null,
         'data-classes' => 'table table-bordered table-hover',
-        'data-thead-classes' => 'undefined',
-        'data-click-to-select' => false,
+        'data-thead-classes' => '', // undefined
+        'data-click-to-select' => false, // clickToSelect decamilize data-click-to-select
         'data-pagination' => false,
         'data-search-highlight' => false,
         'data-show-columns' => false,
@@ -26,7 +138,34 @@ class GUI_Table extends GUI_Module
         'data-show-toggle' => false,
         'data-sortable' => true,
         'data-search' => false,
-        'data-side-pagination' => 'client'
+        'data-side-pagination' => 'client',
+        'data-buttons-align' => 'right',
+        'data-buttons-class' => 'secondary',
+        'data-buttons-order' => '[\'paginationSwitch\', \'refresh\', \'toggle\', \'fullscreen\', \'columns\']', // array
+        'data-buttons-prefix' => 'btn',
+        'data-buttons-toolbar' => '', // undefined
+        'data-cache' => true,
+        'data-card-view' => false,
+        'data-checkbox-header' => true,
+        'data-content-type' => 'application/json',
+        'data-data' => '[]', // Array | Object
+        'data-data-field' => 'rows',
+        'data-data-type' => 'json',
+        'data-detail-view' => false,
+        'data-detail-view-align' => 'left',
+        'data-detail-view-by-click' => false,
+        'data-detail-view-icon' => true,
+        'data-escape' => false,
+        'data-filter-options' => '{ filterAlgorithm: \'and\' }',
+
+        // functions
+        'data-ajax' => '', // undefined
+        'data-ajax-options' => '', // {}
+        'data-buttons' => '', // {}
+        'data-custom-search' => '', // undefined
+        'data-custom-sort' => '', // undefined
+        'data-detail-filter' => 'function(index, row) { return true }',
+        'data-detail-formatter' => 'function(index, row, element) { return \'\' }',*/
     ];
 
     protected array $columns = [];
@@ -37,7 +176,7 @@ class GUI_Table extends GUI_Module
     public function init($superglobals = I_EMPTY)
     {
         $this->Defaults->addVar('framework', 'bs4');
-        $this->Defaults->addVar('data-url', null);
+        $this->Defaults->addVar('url', null);
         $this->Defaults->addVar('columns', null);
         parent::init($superglobals);
     }
@@ -45,11 +184,11 @@ class GUI_Table extends GUI_Module
     /**
      * Load files
      *
-     * @throws ReflectionException
+     * @throws ReflectionException|Exception
      */
     public function loadFiles()
     {
-        $className = strtolower($this->getClassName());
+        $className = strtolower(__CLASS__);
         $fw = $this->getVar('framework');
         $tpl = $this->Weblication->findTemplate('tpl_table_'.$fw.'.html', $className, true);
         $this->Template->setFilePath('stdout', $tpl);
@@ -60,13 +199,76 @@ class GUI_Table extends GUI_Module
         }
     }
 
-    public function setOptions(array $options)
+    public function setOptions(array $options): GUI_Table
     {
-        $this->options = array_merge($this->options, $options);
+        foreach($options as $key => $value) {
+            if(isset($this->defaultOptions[$key])) {
+                if($this->defaultOptions[$key]['value'] != $value) {
+                    $this->options[$key] = $value;
+                }
+            }
+        }
+
+        return $this;
     }
 
+    public function setColumns(array $columns): GUI_Table
+    {
+        foreach($columns as $z => $column) {
+            foreach($column as $key => $value)
+                if(isset($this->defaultColumnOptions[$key])) {
+                    if($this->defaultColumnOptions[$key]['value'] != $value) {
+                        $this->columns[$z][$key] = $value;
+                    }
+                }
+        }
+
+//        $this->columns = $columns;
+        return $this;
+    }
+
+    public function loadConfig(string $json): bool
+    {
+        $result = false;
+        $data = json_decode($json, JSON_OBJECT_AS_ARRAY);
+        if(json_last_error() != JSON_ERROR_NONE) {
+            return false;
+        }
+        if(isset($data['options'])) {
+            $this->options = $data['options'];
+            $result = true;
+        }
+        if(isset($data['columns'])) {
+            $this->columns = $data['columns'];
+            $result = true;
+        }
+        return $result;
+    }
+
+    public function getConfig(): string
+    {
+        $data = [
+            'options' => $this->options,
+            'columns' => $this->columns
+        ];
+        return json_encode($data);
+    }
+
+    /**
+     * Provisioning data before preparing module and there children.
+     */
     public function provision()
     {
+        $data = $this->Input->getData();
+        unset(
+            $data['moduleName'],
+            $data['ModuleName'],
+            $data['modulename'],
+            $data['framework']
+        );
+
+        $this->setOptions($data);
+
         if($this->Input->getVar('columns') != null) {
             $columns = $this->Input->getVar('columns');
             switch (gettype($columns)) {
@@ -83,15 +285,25 @@ class GUI_Table extends GUI_Module
      */
     public function prepare()
     {
-        $this->Template->setVar('moduleName', $this->getName());
+        $this->Template->setVar([
+            'moduleName' => $this->getName(),
+            'className' => $this->getClassName()
+        ]);
+
 
         $this->Template->newBlock('tableAttributes');
-        foreach($this->options as $attrName => $attrValue) {
-            $inpValue = $this->Input->getVar($attrName);
-            $attrValue = $inpValue ?? $attrValue;
+        foreach($this->options as $optName => $attrValue) {
+//            $inpValue = $this->Input->getVar($attrName);
+//            $attrValue = $inpValue ?? $attrValue;
+            $attrName = $this->defaultOptions[$optName]['attribute'];
+            $attrType = $this->defaultOptions[$optName]['type'];
 
             if(is_bool($attrValue)) {
                 $attrValue = bool2string($attrValue);
+            }
+
+            if($attrValue == '') {
+                continue;
             }
 
             $TableAttributeBlock = $this->Template->newBlock('tableAttribute');
@@ -101,17 +313,43 @@ class GUI_Table extends GUI_Module
                 ]
             );
         }
+        unset($optName);
 
-        $this->Template->newBlock('row');
+//        $this->Template->newBlock('row');
+//        foreach($this->columns as $column) {
+//            $ColumnBlock = $this->Template->newBlock('column');
+//            $ColumnBlock->setVar('caption', $column['caption'] ?? '');
+//            unset($column['caption']);
+//            foreach($column as $attrName => $attrValue) {
+//                $ColumnAttributeBlock = $this->Template->newBlock('columnAttribute');
+//                $ColumnAttributeBlock->setVar([
+//                    'data-name' => $attrName,
+//                    'data-value' => $attrValue
+//                ]);
+//            }
+//        }
+
+        $this->Template->newBlock('js_row');
         foreach($this->columns as $column) {
-            $ColumnBlock = $this->Template->newBlock('column');
-            $ColumnBlock->setVar('caption', $column['caption'] ?? '');
-            unset($column['caption']);
-            foreach($column as $attrName => $attrValue) {
-                $ColumnAttributeBlock = $this->Template->newBlock('columnAttribute');
+            $ColumnBlock = $this->Template->newBlock('js_column');
+            foreach($column as $optName => $attrValue) {
+                $type = $this->defaultColumnOptions[$optName]['type'];
+
+                switch($type) {
+                    case 'boolean':
+                        $attrValue = bool2string($attrValue);
+                        break;
+
+                    case 'function':
+                        break;
+
+                    default:
+                        $attrValue = '\''.$attrValue.'\'';
+                }
+                $ColumnAttributeBlock = $this->Template->newBlock('js_columnOption');
                 $ColumnAttributeBlock->setVar([
-                    'data-name' => $attrName,
-                    'data-value' => $attrValue
+                    'key' => $optName,
+                    'value' => str_replace('{modulename}', $this->getName(), $attrValue)
                 ]);
             }
         }
--- guis/gui_table/table.js
+++ guis/gui_table/table.js
@@ -5,9 +5,11 @@
  *
  * @author Alexander Manhart <alexander@manhart-it.de>
  */
+
 'use strict';
 
-class Table {
+// $.BootstrapTable = class extends $.BootstrapTable {}
+class GUI_Table {
     /* > ES7
     static const STYLE_DEFAULT = 'toast';
     static const STYLE_ERROR = 'error';
@@ -15,23 +17,92 @@ class Table {
     static const STYLE_SUCCESS = 'success';
     static const STYLE_WARNING = 'warning';
     */
-    dataUrl = '';
+    url = '';
+
+    table = null;
+
+    moduleName = 'GUI_Table';
+
+    dateTimeFormat = '%Y-%m-%d %H:%M';
+
+    options = [];
 
     /**
      * Defaults
      *
      * @constructor
      */
-    constructor()
+    constructor(options = {})
     {
+        this.moduleName = options['moduleName'];
+        delete options['moduleName'];
+        this.options = options;
+        // this.columns = options['columns'];
+
+        this.table = $('#'+this.moduleName)
+
+        // let columns = {
+        //     columns: [{
+        //         field: 'idLoginProtocol',
+        //         title: 'ID',
+        //     }, {
+        //         field: 'username',
+        //         title: 'Benutzer',
+        //     }, {
+        //         field: 'loginDateTime',
+        //         title: 'eingeloggt am',
+        //         formatter: this.formatDateTime
+        //     }]
+        // }
+        // this.table.bootstrapTable(columns);
+
+        //this.table.bootstrapTable('refreshOptions', options);
+    }
+
+    setColumns(columns = [])
+    {
+        this.columns = columns;
+        this.options['columns'] = this.columns;
+        return this;
+    }
+
+    render()
+    {
+        this.table.bootstrapTable(
+            this.options
+        );
+        return this;
+    }
+
+    sortDateTime(a, b)
+    {
+        if (new Date(a) > new Date(b)) return 1;
+        if (new Date(a) < new Date(b)) return -1;
+        return 0;
+    }
+
+    formatDateTime(value, row, index, field)
+    {
+        let format = '%Y-%m-%d %H:%M';
+        // console.debug('format: '+format);
+        // console.debug(this);
+        // console.debug('dateTimeFOrmat:',this.dateTimeFormat);
+        return new Date(value).strftime(format);
     }
 }
+
+/*
+$.extend($.fn.bootstrapTable.defaults.icons, {
+    clearSearch: 'fa-undo'
+});
+*/
+
 
 /**
  * Example e.g. for testing:
  *
  * */
-ready(function () {
-});
+// ready(function () {
+// });
 
 console.debug('table.js loaded');
