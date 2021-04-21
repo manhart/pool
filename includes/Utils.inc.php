b8bbf0eedbfb5369e9ddd3897f4f2ffb2f1c60c4
Alexander Manhart <alexander@manhart-it.de>
2021-04-21 16:05:13 +0200

    Headerdata extended by attributes, new fct htmlAttributes in utils.inc.php

--- includes/Utils.inc.php
+++ includes/Utils.inc.php
@@ -4614,3 +4614,19 @@ function decamelize(string $string, $separator = '-'): string
     return preg_replace('/\B([A-Z])/', $separator.'$0', $string);
     // alternate (todo: test speed) return strtolower(preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1'.$separator.'$2', $string));
 }
+
+/**
+ * array to html attributes
+ *
+ * @param array $attributes
+ * @return string
+ */
+function htmlAttributes(array $attributes): string
+{
+    $attributes = implode(' ', array_map(function ($k, $v) {
+        if(is_array($v)) $v = implode(' ', $v);
+        return ((is_int($k)) ? $v : $k .'="'. htmlspecialchars($v) .'"');
+    }, array_keys($attributes), $attributes));
+    if($attributes) $attributes = ' '.$attributes;
+    return $attributes;
+}
