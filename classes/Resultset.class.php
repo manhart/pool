7b4a5b8d61508af4dba01b7c1061160fcc4439f8
Alexander Manhart <alexander@manhart-it.de>
2021-05-17 15:06:38 +0200

    fix empty datetime

--- classes/Resultset.class.php
+++ classes/Resultset.class.php
@@ -424,6 +424,9 @@ if(!defined('CLASS_RESULTSET')) {
         public function getValueAsDateTime(string $key, $default=null, ?DateTimeZone $timezone=null): ?DateTime
         {
             $value = $this->getValue($key, $default);
+            if($value === '' or $value === '0000-00-00' or $value === '0000-00-00 00:00:00') {
+                return null;
+            }
             if(is_null($value) == false) {
                 if(strpos($value, '-') === false) {
                     $value = '@'.$value; // should be an unix timestamp (integer)
