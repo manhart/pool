c88d9945eea94910995067afe3cd8de21d920c64
Alexander Manhart <alexander@manhart-it.de>
2021-06-11 08:16:23 +0200

    trait Configurable

---
+++ classes/Configurable.class.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * g7system.local
+ *
+ * Configurable.class.php created at 10.06.21, 11:23
+ *
+ * @author A.Manhart <A.Manhart@group-7.de>
+ * @copyright Copyright (c) 2021, GROUP7 AG
+ */
+
+
+trait Configurable {
+
+    public function setup(array $settings)
+    {
+
+    }
+
+    public function loadConfiguration()
+    {
+
+    }
+
+    public function saveConfiguration()
+    {
+
+    }
+}
