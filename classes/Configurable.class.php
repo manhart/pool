6210b33f3eb796d13aa94a3592e160b4fd3e28e3
Alexander Manhart <alexander@manhart-it.de>
2021-06-25 16:58:35 +0200

    big update Configurable Modules, ModuleConfigurator...

--- classes/Configurable.class.php
+++ classes/Configurable.class.php
@@ -8,21 +8,20 @@
  * @copyright Copyright (c) 2021, GROUP7 AG
  */
 
-namespace pool\classes;
-
-trait Configurable {
-    protected array $configurationKeys = [];
-    protected string $storageEngine = '';
-    public function getStorageEngine(): string
-    {
-        return $this->storageEngine;
-    }
-    public function setupConfiguration(array $keys) {
-        $this->configurationKeys = $keys;
-    }
-    public abstract function hasConfiguration();
-    public abstract function loadConfiguration();
-    public abstract function saveConfiguration();
-    public abstract function getConfiguration(): array;
-    public abstract function setConfiguration(array $configuration);
-}
+//
+//trait Configurable {
+//    protected array $configurationKeys = [];
+//    protected string $storageEngine = '';
+//    public function getStorageEngine(): string
+//    {
+//        return $this->storageEngine;
+//    }
+//    public function setupConfiguration(array $keys) {
+//        $this->configurationKeys = $keys;
+//    }
+//    public abstract function hasConfiguration();
+//    public abstract function loadConfiguration();
+//    public abstract function saveConfiguration();
+//    public abstract function getConfiguration(): array;
+//    public abstract function setConfiguration(array $configuration);
+//}
