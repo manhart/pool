1787b0a292ff33a35be5b31c329c585127c08ebf
Alexander Manhart <alexander@manhart-it.de>
2021-12-14 08:46:11 +0100

    changed old define style to const

--- pool.lib.php
+++ pool.lib.php
@@ -1,31 +1,31 @@
 <?php
 /**
- * -= PHP Object Oriented Library (POOL) =-
+ * POOL
  *
- * pool.lib.php
+ * [P]HP [O]bject-[O]riented [L]ibrary
  *
- * Inkludiert alle benoetigten Basisdateien fuer die Verwendung der Rapid Module Library!
+ * Includes all necessary base files for the use of POOL
  *
  * @version $Id: pool.lib.php,v 1.2 2005/06/03 08:32:08 manhart Exp $
  * @version $Revision 1.0$
  * @version
  *
  * @since 2003-09-30
- * @author Alexander Manhart <alexander@manhart.bayern>
+ * @author Alexander Manhart <alexander@manhart-it.de>
  * @link https://alexander-manhart.de
  */
 
-define('POOL', 'POOL');
+const POOL = 'POOL';
 
-# Konstanten f�r die Unterverzeichnisse
-define('PWD_TILL_INCLUDES', 'includes');
-define('PWD_TILL_CLASSES', 'classes');
-define('PWD_TILL_GUIS', 'guis');
-define('PWD_TILL_SCHEMES', 'schemes');
-define('PWD_TILL_SKINS', 'skins');
-define('PWD_TILL_JAVASCRIPTS', 'javascripts');
-define('PWD_TILL_3RDPARTY', '3rdparty');
-define('PWD_TILL_SUBCODES', 'subcodes');
+// subdirectories
+const PWD_TILL_INCLUDES = 'includes';
+const PWD_TILL_CLASSES = 'classes';
+const PWD_TILL_GUIS = 'guis';
+const PWD_TILL_SCHEMES = 'schemes';
+const PWD_TILL_SKINS = 'skins';
+const PWD_TILL_JAVASCRIPTS = 'javascripts';
+// const PWD_TILL_3RDPARTY = '3rdparty';
+const PWD_TILL_SUBCODES = 'subcodes';
 
 if(!defined('DIR_POOL_ROOT')) {
     define('DIR_POOL_ROOT', __DIR__);
