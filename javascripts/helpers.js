9bb35c6a5aa9b85b456d4c740182c10565d2a584
Alexander Manhart <alexander@manhart-it.de>
2021-12-16 11:25:01 +0100

    babel/helpers - typeof

--- javascripts/helpers.js
+++ javascripts/helpers.js
@@ -2359,3 +2359,25 @@ function htmlspecialchars(string, quoteStyle, charset, doubleEncode)
     }
     return string
 }
+
+/**
+ * babel/helpers - typeof
+ *
+ * @param obj
+ * @returns {string}
+ */
+function _typeof(obj)
+{
+    if (typeof Symbol === 'function' && typeof Symbol.iterator === 'symbol') {
+        _typeof = function (obj) {
+            return typeof obj;
+        };
+    }
+    else {
+        _typeof = function (obj) {
+            return obj && typeof Symbol === 'function' && obj.constructor === Symbol && obj !== Symbol.prototype ? 'symbol' : typeof obj;
+        };
+    }
+
+    return _typeof(obj);
+}
