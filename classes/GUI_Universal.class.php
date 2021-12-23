1dfe7e50705a09573cefb82908714d11696e8ab5
Alexander Manhart <alexander@manhart-it.de>
2021-12-23 11:48:03 +0100

    GUI_Universal reworked and Bugs eliminated;

--- classes/GUI_Universal.class.php
+++ classes/GUI_Universal.class.php
@@ -41,35 +41,35 @@ class GUI_Universal extends GUI_Module
      *
      * @access public
      **/
-    function init($superglobals=I_EMPTY)
+    function init($superglobals = I_EMPTY)
     {
         $this->Defaults->addVar(
             array(
                 /* Allgemeine Universalattribute */
-                'id' 			=> $this->getName(),
-                'title'			=> '',
-                'style'			=> null,
-                'class'			=> $this->getClassName(),
-                'class_error'	=> $this->getClassName() . '_error',
-                'attributes'	=> '',
+                'id' => $this->getName(),
+                'title' => '',
+                'style' => null,
+                'class' => $this->getClassName(),
+                'class_error' => $this->getClassName().'_error',
+                'attributes' => '',
 
                 /* Universalattribute zur Internationalisierung */
-                'dir'			=> 'ltr',
-                'lang'			=> null,
+                'dir' => 'ltr',
+                'lang' => null,
 
                 /* Universalattribute fuer Event-Handler */
-                'onclick'		=> '',
-                'ondblclick'	=> '',
-                'onmousedown'	=> '',
-                'onmouseup'		=> '',
-                'onmouseover'	=> '',
-                'onmousemove'	=> '',
-                'onmouseout'	=> '',
-                'onkeypress'	=> '',
-                'onkeydown'		=> '',
-                'onkeyup'		=> '',
+                'onclick' => '',
+                'ondblclick' => '',
+                'onmousedown' => '',
+                'onmouseup' => '',
+                'onmouseover' => '',
+                'onmousemove' => '',
+                'onmouseout' => '',
+                'onkeypress' => '',
+                'onkeydown' => '',
+                'onkeyup' => '',
 
-                'guierror' 		=> null
+                'guierror' => null
             )
         );
 
@@ -77,132 +77,115 @@ class GUI_Universal extends GUI_Module
     }
 
     /**
-     * GUI_Universal::prepare()
-     *
-     * @return
+     * main logic
      **/
-    function prepare ()
+    public function prepare()
     {
         #### Bindet gui_....css ein:
-        $cssfile = @$this->Weblication->findStyleSheet($this->getClassName().'.css', $this->getClassName(), true);
-        if ($cssfile) {
-            /*if(version_compare(phpversion(), '5.0.0', '>=')) {
-                if ($this->Weblication->Main instanceof GUI_Module) {
-                    if (isset($this -> Weblication -> Main -> Headerdata) and is_a($this -> Weblication -> Main -> Headerdata, 'GUI_Headerdata')) {
-                        $this -> Weblication -> Main -> Headerdata -> addStyleSheet($cssfile);
-                    }
-                }
-            }
-            else {*/
-                if (is_a($this -> Weblication -> Main, 'GUI_Module')) {
-                    if (isset($this -> Weblication -> Main -> Headerdata) and is_a($this -> Weblication -> Main -> Headerdata, 'GUI_Headerdata')) {
-                        $this -> Weblication -> Main -> Headerdata -> addStyleSheet($cssfile);
-                    }
-                }
-            /*}*/
+        if($this->Weblication->hasFrame()) {
+            $cssFile = @$this->Weblication->findStyleSheet($this->getClassName().'.css', $this->getClassName(), true);
+            $this->Weblication->getFrame()->getHeaderdata()->addStyleSheet($cssFile);
         }
 
-        $Input = & $this -> Input;
-
-        $this->id = $Input->getVar('id');
+        $this->id = $this->Input->getVar('id');
 
 
-        $class = $Input -> getVar('class');
-        $class_error = $Input -> getVar('class_error');
-        $guierror = $Input->getVar('guierror');
-        if ($guierror and $guierror == $this->Input->getVar('name')) {
+        $class = $this->Input->getVar('class');
+        $class_error = $this->Input->getVar('class_error');
+        $guierror = $this->Input->getVar('guierror');
+        if($guierror and $guierror == $this->Input->getVar('name')) {
             $class = $class_error;
         }
         #### Events
         $events = '';
-        $onclick = $Input->getVar('onclick');
-        if ($onclick) {
-            $events .= 'onclick="' . $onclick . '"';
+        $onclick = $this->Input->getVar('onclick');
+        if($onclick) {
+            $events .= 'onclick="'.$onclick.'"';
         }
-        $ondblclick = $Input -> getVar('ondblclick');
-        if ($ondblclick) {
+        $ondblclick = $this->Input->getVar('ondblclick');
+        if($ondblclick) {
             $events .= ' ';
-            $events .= 'ondblclick="' . $ondblclick . '"';
+            $events .= 'ondblclick="'.$ondblclick.'"';
         }
-        $onmousedown = $Input->getVar('onmousedown');
-        if ($onmousedown) {
+        $onmousedown = $this->Input->getVar('onmousedown');
+        if($onmousedown) {
             $events .= ' ';
-            $events .= 'onmousedown="' . $onmousedown . '"';
+            $events .= 'onmousedown="'.$onmousedown.'"';
         }
-        $onmouseup = $Input->getVar('onmouseup');
-        if ($onmouseup) {
+        $onmouseup = $this->Input->getVar('onmouseup');
+        if($onmouseup) {
             $events .= ' ';
-            $events .= 'onmouseup="' . $onmouseup . '"';
+            $events .= 'onmouseup="'.$onmouseup.'"';
         }
-        $onmouseover = $Input->getVar('onmouseover');
-        if ($onmouseover) {
+        $onmouseover = $this->Input->getVar('onmouseover');
+        if($onmouseover) {
             $events .= ' ';
-            $events .= 'onmouseover="' . $onmouseover . '"';
+            $events .= 'onmouseover="'.$onmouseover.'"';
         }
-        $onmousemove = $Input->getVar('onmousemove');
-        if ($onmousemove) {
+        $onmousemove = $this->Input->getVar('onmousemove');
+        if($onmousemove) {
             $events .= ' ';
-            $events .= 'onmousemove="' . $onmousemove . '"';
+            $events .= 'onmousemove="'.$onmousemove.'"';
         }
-        $onmouseout = $Input->getVar('onmouseout');
-        if ($onmouseout) {
+        $onmouseout = $this->Input->getVar('onmouseout');
+        if($onmouseout) {
             $events .= ' ';
-            $events .= 'onmouseout="' . $onmouseout . '"';
+            $events .= 'onmouseout="'.$onmouseout.'"';
         }
-        $onkeypress = $Input->getVar('onkeypress');
-        if ($onkeypress) {
+        $onkeypress = $this->Input->getVar('onkeypress');
+        if($onkeypress) {
             $events .= ' ';
-            $events .= 'onkeypress="' . $onkeypress . '"';
+            $events .= 'onkeypress="'.$onkeypress.'"';
         }
-        $onkeydown = $Input->getVar('onkeydown');
-        if ($onkeydown) {
+        $onkeydown = $this->Input->getVar('onkeydown');
+        if($onkeydown) {
             $events .= ' ';
-            $events .= 'onkeydown="' . $onkeydown . '"';
+            $events .= 'onkeydown="'.$onkeydown.'"';
         }
-        $onkeyup = $Input->getVar('onkeyup');
-        if ($onkeyup) {
+        $onkeyup = $this->Input->getVar('onkeyup');
+        if($onkeyup) {
             $events .= ' ';
-            $events .= 'onkeyup="' . $onkeyup . '"';
+            $events .= 'onkeyup="'.$onkeyup.'"';
         }
 
-        $this -> events .= $events;
+        $this->events .= $events;
 
         #### Universal Attribute
         $attributes = '';
-        $title = $Input->getVar('title');
-        if ($title) {
-            $attributes .= 'title="' . $title . '"';
+        $title = $this->Input->getVar('title');
+        if($title) {
+            $attributes .= 'title="'.$title.'"';
         }
-        $style = $Input->getVar('style');
-        if ($style) {
+        $style = $this->Input->getVar('style');
+        if($style) {
             $attributes .= ' ';
-            $attributes .= 'style="' . $style . '"';
+            $attributes .= 'style="'.$style.'"';
         }
-        if ($class) {
+        if($class) {
             $attributes .= ' ';
-            $attributes .= 'class="' . $class . '"';
+            $attributes .= 'class="'.$class.'"';
         }
-        if ($class_error) {
+        if($class_error) {
             $attributes .= ' ';
-            $attributes .= 'class_error="' . $class_error . '"';
+            $attributes .= 'class_error="'.$class_error.'"';
         }
         #### Universal Attribute f�r Internationalisierung
-        $lang = $Input->getVar('lang');
-        if ($lang) {
-           $attributes .= ' ';
-           $attributes .= 'lang="' . $lang .'"';
-        }
-        $dir = $Input->getVar('dir');
-        if ($dir) {
+        $lang = $this->Input->getVar('lang');
+        if($lang) {
             $attributes .= ' ';
-            $attributes .= 'dir="' . $dir . '"';
+            $attributes .= 'lang="'.$lang.'"';
         }
-        $custom_attributes = $Input -> getVar('attributes');
-        if ($custom_attributes) {
+        $dir = $this->Input->getVar('dir');
+        if($dir) {
+            $attributes .= ' ';
+            $attributes .= 'dir="'.$dir.'"';
+        }
+        $custom_attributes = $this->Input->getVar('attributes');
+        if($custom_attributes) {
             $attributes .= ' ';
             $attributes .= $custom_attributes;
         }
 
-        $this -> attributes .= $attributes;
+        $this->attributes .= $attributes;
     }
 }
