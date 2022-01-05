10fcf8d7d3cdfd4f7c1a91916bddbd400f9cfc53
Alexander Manhart <alexander@manhart-it.de>
2022-01-05 09:36:08 +0100

    conversion of data types error fixed

--- classes/GUI_Module.class.php
+++ classes/GUI_Module.class.php
@@ -605,18 +605,20 @@ class GUI_Module extends Module
             foreach($parameters as $Parameter) {
                 if($this->Input->exists($Parameter->getName())) {
                     $value = $this->Input->getVar($Parameter->getName());
-                    switch($Parameter->getType()->getName()) {
-                        case 'float':
-                            $value = (float)$value;
-                            break;
+                    if(is_string($value)) {
+                        switch($Parameter->getType()->getName()) {
+                            case 'float':
+                                $value = (float)$value;
+                                break;
 
-                        case 'int':
-                            $value = (int)$value;
-                            break;
+                            case 'int':
+                                $value = (int)$value;
+                                break;
 
-                        case 'bool':
-                            $value = string2bool($value);
-                            break;
+                            case 'bool':
+                                $value = string2bool($value);
+                                break;
+                        }
                     }
                     $args[] = $value;
                 }
