66488ba92884b9204c59cd275a939b31d34f47b0
Alexander Manhart <alexander@manhart-it.de>
2022-01-28 12:25:44 +0100

    Log reworked

--- classes/Log.class.php
+++ classes/Log.class.php
@@ -14,292 +14,282 @@
  * @package pool
  */
 
-if(!defined('CLASS_LOG')) {
+/**
+ * Die Grundklasse, der Uhrahn aller Objekte.
+ *
+ * Die Klasse Log verf�gt �ber folgende Verhalten:
+ * - stellt eine Art Debug-Modus bereit.
+ * - Objektinstanzen erzeugen, verwalten und aufl�sen.
+ * - auf objektspezifische Informationen �ber den Klassentyp und die Instanz zugreifen.
+ * - enth�lt Fehler�berpr�fung und kann Fehler ausl�sen.
+ * - stellt ein Verfahren bereit mit dem ein Inhalt eines Objekts einem anderen zugewiesen werden kann.
+ *
+ * Log wird nie direkt instantiiert. Obwohl keine Programmiersprachenelemente zum Verhindern der Instantiierung verwendet werden, ist Log eine abstrakte Klasse.
+ *
+ * @author Alexander Manhart <alexander@manhart-it.de>
+ */
+class Log extends PoolObject
+{
     /**
-     * Verhindert mehrfach Einbindung der Klassen (prevent multiple loading)
-     * @ignore
+     * Datei
+     *
+     * @var string $file
      */
-    define('CLASS_LOG',			1);
+    private string $file='';
+
     /**
-     * Die Grundklasse, der Uhrahn aller Objekte.
+     * Datei-Resource
      *
-     * Die Klasse Log verf�gt �ber folgende Verhalten:
-     * - stellt eine Art Debug-Modus bereit.
-     * - Objektinstanzen erzeugen, verwalten und aufl�sen.
-     * - auf objektspezifische Informationen �ber den Klassentyp und die Instanz zugreifen.
-     * - enth�lt Fehler�berpr�fung und kann Fehler ausl�sen.
-     * - stellt ein Verfahren bereit mit dem ein Inhalt eines Objekts einem anderen zugewiesen werden kann.
+     * @access private
+     * @var resource $fp
+     */
+    var $fp=null;
+
+    /**
+     * Logfiles rotieren (Standard ausgeschaltet)
      *
-     * Log wird nie direkt instantiiert. Obwohl keine Programmiersprachenelemente zum Verhindern der Instantiierung verwendet werden, ist Log eine abstrakte Klasse.
+     * @access private
+     * @var boolean
+     */
+    var $logRotate=false;
+
+    /**
+     * Maximale Dateigr��e des Logfiles (standard 2 MB)
+     *
+     * @access private
+     * @var string
+     */
+    var $maxFileSize=2097152;
+
+    /**
+     * Rotiert die Datei beginnend mit einem neuen Tag
+     *
+     * @var boolean
+     */
+    var $rotateByDate = false;
+
+    /**
+     * Trennzeichen
+     *
+     * @access private
+     * @var string
+     */
+    var $separator = "\t";
+
+    /**
+     * Zeilenumbruch
+     *
+     * @access private
+     * @var string
+     */
+    var $lineFeed = "\n";
+
+    /**
+     * Speichert die geschriebenen Zeilen zwischen
+     *
+     * @var array
+     */
+    var $cache = array();
+
+    /**
+     * Cache aktiviert
+     *
+     * @var boolean
+     */
+    protected $enableCache = false;
+
+    var $sid = 0000;
+    var $withSID = true;
+
+    /**
+     * Format des Zeitstempels
+     *
+     * @var string
+     */
+    var $formatDateTime = '[%d.%m.%Y %H:%M:%S]';
+
+    /**
+     * Konstruktor
      *
      * @access public
-     * @author Alexander Manhart <alexander.manhart@freenet.de>
-     * @package pool
      */
-    class Log extends PoolObject
+    public function __construct()
     {
-        /**
-         * Datei
-         *
-         * @access private
-         * @var string $file
-         */
-        var $file='';
+        $this->sid = rand(0, 9999);
+    }
 
-        /**
-         * Datei-Resource
-         *
-         * @access private
-         * @var resource $fp
-         */
-        var $fp=null;
+    /**
+     * @param bool $rotateByDate
+     */
+    function activateLogRotate($rotateByDate = false)
+    {
+        $this->logRotate = true;
+        $this->rotateByDate = $rotateByDate;
+    }
 
-        /**
-         * Logfiles rotieren (Standard ausgeschaltet)
-         *
-         * @access private
-         * @var boolean
-         */
-        var $logRotate=false;
+    /**
+     * Deaktiviert Log-Session
+     *
+     */
+    function disableSID()
+    {
+        $this->withSID = false;
+    }
 
-        /**
-         * Maximale Dateigr��e des Logfiles (standard 2 MB)
-         *
-         * @access private
-         * @var string
-         */
-        var $maxFileSize=2097152;
-
-        /**
-         * Rotiert die Datei beginnend mit einem neuen Tag
-         *
-         * @var boolean
-         */
-        var $rotateByDate = false;
-
-        /**
-         * Trennzeichen
-         *
-         * @access private
-         * @var string
-         */
-        var $separator = "\t";
-
-        /**
-         * Zeilenumbruch
-         *
-         * @access private
-         * @var string
-         */
-        var $lineFeed = "\n";
-
-        /**
-         * Speichert die geschriebenen Zeilen zwischen
-         *
-         * @var array
-         */
-        var $cache = array();
-
-        /**
-         * Cache aktiviert
-         *
-         * @var boolean
-         */
-        protected $enableCache = false;
-
-        var $sid = 0000;
-        var $withSID = true;
-
-        /**
-         * Format des Zeitstempels
-         *
-         * @var string
-         */
-        var $formatDateTime = '[%d.%m.%Y %H:%M:%S]';
-
-        /**
-         * Konstruktor
-         *
-         * @access public
-         */
-        public function __construct()
-        {
-            $this->sid = rand(0, 9999);
-        }
-
-        /**
-         * @param bool $rotateByDate
-         */
-        function activateLogRotate($rotateByDate = false)
-        {
-            $this->logRotate = true;
-            $this->rotateByDate = $rotateByDate;
-        }
-
-        /**
-         * Deaktiviert Log-Session
-         *
-         */
-        function disableSID()
-        {
-            $this->withSID = false;
-        }
-
-        /**
-         * �ffnet Logfile
-         *
-         * @param string $file
-         */
-        function open($file, $mode='a')
-        {
-            $this->file = $file;
-            if(file_exists($file)) {
-                $filesize = filesize($file);
-                if($this->logRotate) {
-                    if($this->rotateByDate) {
-                        $newDate = (floor(filemtime($file)/86400) != floor(time()/86400));
-                        if($newDate) $this->rotate($file);
-                    }
-                    else if($filesize > $this->maxFileSize) {
-                        $this->rotate($file);
-                    }
-                    // $mode = 'w';
+    /**
+     * �ffnet Logfile
+     *
+     * @param string $file
+     */
+    function open($file, $mode='a')
+    {
+        $this->file = $file;
+        if(file_exists($file)) {
+            $filesize = filesize($file);
+            if($this->logRotate) {
+                if($this->rotateByDate) {
+                    $newDate = (floor(filemtime($file)/86400) != floor(time()/86400));
+                    if($newDate) $this->rotate($file);
                 }
-            }
-            else {
+                else if($filesize > $this->maxFileSize) {
+                    $this->rotate($file);
+                }
                 // $mode = 'w';
             }
-            $this->_open($mode);
+        }
+        else {
+            // $mode = 'w';
+        }
+        $this->_open($mode);
+    }
+
+    /**
+     * Rotiert das Logfile
+     *
+     * @param string $file
+     */
+    function rotate($file)
+    {
+        $reOpen = false;
+        if($this->isLogging()) {
+            $reOpen = true;
+            $this->close();
         }
 
-        /**
-         * Rotiert das Logfile
-         *
-         * @param string $file
-         */
-        function rotate($file)
-        {
-            $reOpen = false;
-            if($this->isLogging()) {
-                $reOpen = true;
-                $this->close();
-            }
-
-            $nr = 1;
-            while(file_exists($file.'.'.$nr . '.gz.tar')) {
-                $nr++;
-            }
-            $Tar = new Tar();
-            if (version_compare(phpversion(), '4.3.0', '>=')) {
-                $Tar->addData(basename($file), file_get_contents($file));
-            }
-            else {
-                $Tar->addFile($file);
-            }
-            $Tar->toTar($file.'.'.$nr.'.gz.tar', true);
-            @unlink($file);
-
-            if($reOpen) {
-                $this->_open();
-            }
+        $nr = 1;
+        while(file_exists($file.'.'.$nr . '.gz.tar')) {
+            $nr++;
         }
-
-        /**
-         * �ffnet die Datei zum Schreiben
-         *
-         * @param string $mode
-         */
-        function _open($mode='a')
-        {
-            $this->fp = fopen($this->file, $mode);
+        $Tar = new Tar();
+        if (version_compare(phpversion(), '4.3.0', '>=')) {
+            $Tar->addData(basename($file), file_get_contents($file));
         }
-
-        /**
-         * Logging aktiviert? bzw. File geoeffnet
-         *
-         * @return boolean
-         */
-        function isLogging()
-        {
-            return ($this->fp != null);
+        else {
+            $Tar->addFile($file);
         }
+        $Tar->toTar($file.'.'.$nr.'.gz.tar', true);
+        @unlink($file);
 
-        /**
-         * Setzt die maximale Dateigroesse
-         *
-         * @param int $fileSize
-         */
-        function setMaxFileSize($fileSize=2097152)
-        {
-            $this->maxFileSize = $fileSize;
+        if($reOpen) {
+            $this->_open();
         }
+    }
 
-        /**
-         * Trennzeichen (Standard \t)
-         *
-         * @param string $separator
-         */
-        function setSeparator($separator)
-        {
-            $this->separator = $separator;
+    /**
+     * �ffnet die Datei zum Schreiben
+     *
+     * @param string $mode
+     */
+    function _open($mode='a')
+    {
+        $this->fp = fopen($this->file, $mode);
+    }
+
+    /**
+     * Logging aktiviert? bzw. File geoeffnet
+     *
+     * @return boolean
+     */
+    function isLogging()
+    {
+        return ($this->fp != null);
+    }
+
+    /**
+     * Setzt die maximale Dateigroesse
+     *
+     * @param int $fileSize
+     */
+    function setMaxFileSize($fileSize=2097152)
+    {
+        $this->maxFileSize = $fileSize;
+    }
+
+    /**
+     * Trennzeichen (Standard \t)
+     *
+     * @param string $separator
+     */
+    function setSeparator($separator)
+    {
+        $this->separator = $separator;
+    }
+
+    /**
+     * Zeilenumbruch setzen (Standard Unix \n)
+     *
+     * @param string $lineFeed
+     */
+    function setLineFeed($lineFeed)
+    {
+        $this->lineFeed = $lineFeed;
+    }
+
+    /**
+     * Setzt das Format f�r den Zeitstempel
+     *
+     * @param string $format
+     */
+    function setFormatDateTime($format='[%d.%m.%Y %H:%M:%S]')
+    {
+        $this->formatDateTime = $format;
+    }
+
+    /**
+     * Zeilenformat-Anfang
+     *
+     * @return string
+     */
+    protected function __lineFormat()
+    {
+        return formatDateTime(time(), $this->formatDateTime) . (($this->withSID) ? $this->separator . '{' .
+            sprintf('%04d', $this -> sid) . '}' : '').$this->separator.'%s';
+    }
+
+    /**
+     * Ergaenzt einen Logeintrag.
+     *
+     * @param string $text
+     * @param string $separator
+     */
+    public function addLine($text)
+    {
+        $text = sprintf($this->__lineFormat(), $text);
+        if($this->enableCache) {
+            $this->cache[] = $text;
         }
+        $text .= $this->lineFeed;
 
-        /**
-         * Zeilenumbruch setzen (Standard Unix \n)
-         *
-         * @param string $lineFeed
-         */
-        function setLineFeed($lineFeed)
-        {
-            $this->lineFeed = $lineFeed;
-        }
+        fwrite($this->fp, $text, strlen($text));
+    }
 
-        /**
-         * Setzt das Format f�r den Zeitstempel
-         *
-         * @param string $format
-         */
-        function setFormatDateTime($format='[%d.%m.%Y %H:%M:%S]')
-        {
-            $this->formatDateTime = $format;
-        }
-
-        /**
-         * Zeilenformat-Anfang
-         *
-         * @return string
-         */
-        protected function __lineFormat()
-        {
-            return formatDateTime(time(), $this->formatDateTime) . (($this->withSID) ? $this->separator . '{' .
-                sprintf('%04d', $this -> sid) . '}' : '').$this->separator.'%s';
-        }
-
-        /**
-         * Ergaenzt einen Logeintrag.
-         *
-         * @param string $text
-         * @param string $separator
-         */
-        public function addLine($text)
-        {
-            $text = sprintf($this->__lineFormat(), $text);
-            if($this->enableCache) {
-                $this->cache[] = $text;
-            }
-            $text .= $this->lineFeed;
-
-            fwrite($this->fp, $text, strlen($text));
-        }
-
-        /**
-         * Logfile schlie�en
-         */
-        public function close()
-        {
-            $this->cache = array();
-            fclose($this->fp);
-            $this->fp = null;
-        }
+    /**
+     * Logfile schlie�en
+     */
+    public function close()
+    {
+        $this->cache = array();
+        fclose($this->fp);
+        $this->fp = null;
     }
 }
