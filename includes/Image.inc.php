8d9a0d11419cca12c0d98252e9c4dc63705098e0
Alexander Manhart <alexander@manhart-it.de>
2022-08-31 13:38:16 +0200

    Added a comment to a useful link (png transparency)

--- includes/Image.inc.php
+++ includes/Image.inc.php
@@ -275,6 +275,7 @@ function resizeImageToBestFit($im, $max_width, $max_height)
     $originalHeight = imagesy($im);
 
     if ($originalWidth <= $max_width and $originalHeight <= $max_height) {
+        imagealphablending($im, false);
         imagesavealpha($im, true);
         return $im;
     }
@@ -292,6 +293,8 @@ function resizeImageToBestFit($im, $max_width, $max_height)
 
 /**
  * changes the dimensions of the image
+ *
+ * https://christianwood.net/posts/png-files-are-complicate/
  *
  * @param resource $im
  * @param int $width
