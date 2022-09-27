e41a535fbf9e114476e5ff24696e35dd1d6f6c16
Philipp Lehfeld <p.lehfeld@group-7.de>
2022-09-27 09:12:58 +0200

    Simplifying GUI_AjaxDemo

---
+++ includes/JS_File.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * g7system.local
+ *
+ * JS_File.php created at 23.09.22, 15:51
+ *
+ * @author p.lehfeld <p.lehfeld@group-7.de>
+ * @copyright Copyright (c) 2022, GROUP7 AG
+ */
+namespace pool\includes\JS_File {
+
+
+
+}
+namespace pool\includes\JS_File\dir{
+    class JS_File{
+        const DIR = null;
+        const VERSION = null;
+
+        function getFile(string $version){
+
+
+        }
+        protected function getPath():string{
+            if (self::DIR == null) return '';
+            else return parent::getPath;
+        }
+    }
+
+    class Common extends JS_File {
+        const DIR =
+    }
+}
