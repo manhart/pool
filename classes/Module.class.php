cd8037328422c5d642eb0d26c50e559cf5673e82
Alexander Manhart <alexander@manhart-it.de>
2022-11-18 08:47:28 +0100

    Module reworked a little bit

--- classes/Module.class.php
+++ classes/Module.class.php
@@ -29,21 +29,21 @@
  *
  * @author Alexander Manhart <alexander@manhart-it.de>
  * @package pool
- **/
+ */
+
 class Module extends Component
 {
     /**
      * Eltern-Objekt (Parent)->auf wem sitze ich fest?
      *
      * @var Module $Parent
-     * @access private
      */
-    var $Parent=null;
+    private Module $Parent;
 
     /**
-     * @var array{Module} $Modules contains all children modules
+     * @var array{Module} $modules contains all children modules
      */
-    protected array $Modules = [];
+    protected array $modules = [];
 
     /**
      * Standardwerte sollten gew�hrleisten, dass das Modul auch ohne Parametrisierung l�uft. Die Standardwerte werden in der Funktion "init" festgelegt und bestimmen das normale Verhalten des Moduls.
@@ -98,7 +98,7 @@ class Module extends Component
     {
         parent::__construct($Owner);
 
-        $this->Modules = [];
+        $this->modules = [];
         $this->Handoff = [];
         $this->Defaults = new Input(I_EMPTY);
         $this->fixedParams = $params;
@@ -145,19 +145,18 @@ class Module extends Component
      * Importiert Variablen vom Elternmodul (durchschleifen von Variablen).
      *
      * @param array $Handoff Liste bestehend aus Variablen
-     * @return bool Erfolgsstatus
-     **/
-    function importHandoff($Handoff)
+     * @return Module Erfolgsstatus
+     */
+    function importHandoff(array $Handoff): self
     {
-        if (!$this->Input instanceof Input) {
-            return false;
+        if (count($Handoff) == 0) {
+            return $this;
         }
 
-        if (count($Handoff) > 0) {
-            $this->addHandoffVar($Handoff);
-            $this->Input->setVars($Handoff);
-        }
-        return true;
+        $this->addHandoffVar($Handoff);
+        $this->Input->setVars($Handoff);
+
+        return $this;
     }
 
     /**
@@ -187,12 +186,6 @@ class Module extends Component
         if ($moduleName != null) {
             $this->setName($moduleName);
         }
-
-        // @deprecated
-        // $disabled = $this->Input->getVar('ModuleDisabled');
-        // if ($disabled == 1) {
-        //   $this->disable();
-        // }
         return true;
     }
 
@@ -237,9 +230,9 @@ class Module extends Component
      * get fixed param
      *
      * @param $param
-     * @return mixed|null
+     * @return mixed
      */
-    public function getFixedParam($param)
+    public function getFixedParam($param): mixed
     {
         return $this->fixedParams[$param] ?? null;
     }
@@ -324,18 +317,19 @@ class Module extends Component
      * @param string $key
      * @return mixed
      */
-    public function getVar(string $key)
+    public function getVar(string $key): mixed
     {
         return $this->Input->getVar($key);
     }
 
     /**
-     * Prueft ob eine Variable im Container Input enthalten ist
+     * Wrapper for Input::emptyVar
      *
      * @param string $key
      * @return boolean
+     * @see Input
      */
-    function emptyVar($key)
+    public function emptyVar(string $key): bool
     {
         return $this->Input->emptyVar($key);
     }
@@ -343,10 +337,9 @@ class Module extends Component
     /**
      * Setzt das Eltern-Modul (Parent = wem gehoere ich?)
      *
-     * @access public
-     * @param Module|null $Parent Klasse vom Typ Module
+     * @param Module $Parent Klasse vom Typ Module
      */
-    function setParent(?Module $Parent)
+    public function setParent(Module $Parent)
     {
         $this->Parent = $Parent;
     }
@@ -354,22 +347,23 @@ class Module extends Component
     /**
      * Gibt das Eltern-Modul (Parent) zurueck.
      *
-     * @access public
-     * @return Module Ergebnis vom Typ Module
+     * @return Module|null Ergebnis vom Typ Module
      */
     public function getParent(): ?Module
     {
-        return $this->Parent;
+        return $this->Parent ?? null;
     }
 
     /**
      * Fuegt ein Modul in den internen Modul-Container ein.
      *
      * @param Module $Module
+     * @return Module
      */
-    public function insertModule(Module $Module)
+    public function insertModule(Module $Module): self
     {
-        $this->Modules[] = $Module;
+        $this->modules[] = $Module;
+        return $this;
     }
 
     /**
@@ -379,17 +373,16 @@ class Module extends Component
      */
     public function removeModule(Module $Module)
     {
-        $new_Modules = Array();
+        $new_Modules = [];
 
         // Rebuild Modules
-        $max = count($this->Modules);
-        for ($i = 0; $i < $max; $i++) {
-            if ($Module != $this->Modules[$i]) {
-                $new_Modules[] = $this->Modules[$i];
+        foreach($this->modules as $SearchedModule) {
+            if ($Module != $SearchedModule) {
+                $new_Modules[] = $SearchedModule;
             }
         }
 
-        $this->Modules = $new_Modules;
+        $this->modules = $new_Modules;
     }
 
     /**
@@ -400,35 +393,28 @@ class Module extends Component
      */
     public function findChild(string $moduleName): ?Module
     {
-        $result = null;
         if($moduleName == '') return null;
 
-        $max = count($this->Modules);
-        for ($i = 0; $i < $max; $i++){
-            if (strcasecmp($this->Modules[$i]->getName(), $moduleName) == 0) {
-                $result = $this->Modules[$i];
-                break;
+        foreach($this->modules as $Module) {
+            if (strcmp($Module->getName(), $moduleName) == 0) {
+                return $Module;
             }
         }
-        return $result;
+        return null;
     }
 
     /**
-     * Modul wird deaktiviert.
-     *
-     * @access public
-     **/
-    function disable()
+     * deactivates module
+     */
+    public function disable()
     {
         $this->enabled = false;
     }
 
     /**
-     * Modul wird aktiviert.
-     *
-     * @access public
-     **/
-    function enable()
+     * enables module
+     */
+    public function enable()
     {
         $this->enabled = true;
     }
