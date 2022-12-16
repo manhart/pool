04aea1085ab7f684dd0a99540e40e4cb0ae5d6f9
Philipp Lehfeld <p.lehfeld@group-7.de>
2022-12-16 17:02:10 +0100

    Remove base-tag from head templates Add first tests for inline-editing support

--- guis/GUI_CustomFrame/GUI_CustomFrame.class.php
+++ guis/GUI_CustomFrame/GUI_CustomFrame.class.php
@@ -87,7 +87,8 @@ class GUI_CustomFrame extends GUI_Module
     public function loadFiles()
     {
         parent::loadFiles();
-
+        $this->HeadData->addStyleSheet($this->Weblication->findStyleSheet('translatorToolInline.css', '', false));
+        $this->HeadData->addJavaScript($this->Weblication->findJavaScript('translatorToolInline.js', '', true));
         $this->HeadData->addJavaScript($this->Weblication->findJavaScript('Weblication.class.js', '', true));
         $this->HeadData->addJavaScript($this->Weblication->findJavaScript('GUI_Module.class.js', '', true));
     }
