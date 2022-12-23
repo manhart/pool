afcd93ac117e843b42227421d012f081d43ed2af
Philipp Lehfeld <p.lehfeld@group-7.de>
2022-12-23 11:01:48 +0100

    Building translatorToolInline -Changed cross-tab communication to postMessage Changed TranslationCache from Constant to Property of Template class

--- javascripts/translatorToolInline.js
+++ javascripts/translatorToolInline.js
@@ -7,23 +7,62 @@
  * @copyright Copyright (c) 2022, GROUP7 AG
  */
 
+
 class translatorToolInline{
     translationMarkerHash = '#trnsl.';
-    translationMarker = 'trnsl.';
-
-    paragraph;
+    translationMarker = 'trnsl';
+    languageStyles  = [];
     constructor() {
-        console.log('attempting to take control');
-        try {
-            this.paragraph = window.opener.document.getElementById('test');
-        } catch (e) {
-            console.error('failed to take control')
-        }
+        const origin = new URL(window.document.referrer).origin;
+        //check that origin is whitelisted
+        //setup receiver
+        window.addEventListener('message', (event) => {
+                if (event.origin !== origin)
+                    return false;
+                let command = event.data.substring(0, event.data.indexOf(':'));
+                let args = event.data.substring(command.length+1);
+                switch (command){
+                    case 'keySwitch':
+                        this.moveToKey(args);
+                        break;
+                    case 'setup':
+                        let langColours = args.split(',');
+                        let i = 0;
+                        for (const langColour of langColours) {
+                            if (this.languageStyles.length === i) {
+                                const htmlStyleElement = document.createElement('style');
+                                this.languageStyles.push(htmlStyleElement);
+                                document.head.appendChild(htmlStyleElement);
+                            }
+                            const htmlStyleElement = this.languageStyles[i];
+                            const language = langColour.substring(0, langColour.indexOf(' '));
+                            const colour = langColour.substring(language.length+1);
+                            const marker = this.translationMarker;
+                            //set CSS style
+                            htmlStyleElement.innerHTML = `a.${marker}:lang(${language}){background-color:${colour}}`;
+                            i++;
+                        }
+                        //clear style elements that aren't needed
+                        for (;i<this.languageStyles.length;i++){
+                            this.languageStyles[i].innerHTML = '';
+                        }
+                        break;
+                }
+            }
+        );
+        //request setup
+        window.opener.postMessage('setup', origin);
         window.addEventListener('hashchange', () => {
                 if (location.hash.startsWith(this.translationMarkerHash)){
                     let key = location.hash.substring(this.translationMarkerHash.length);
-                    this.editorSetKey(key);//handle refusal?
-                    window.document.getElementById(location.hash.substring(1)).focus();
+                    //send key-switch-request
+                    window.opener.postMessage('keySwitch:'+key, origin);
+                    // this.editorSetKey(key);//handle refusal?
+                    const newTarget = window.document.getElementById(location.hash.substring(1));
+                    if (newTarget)
+                    newTarget.focus();
+                    else
+                        window.opener.postMessage('keyNotOnPage:'+key, origin);
                 }
             }
             , false);
@@ -33,10 +72,6 @@ class translatorToolInline{
         document.location.hash = this.translationMarkerHash +key;
     }
 
-    editorSetKey(key) {
-        this.paragraph.innerHTML = key;
-        console.log(`Set Editor Key to: ${key}`);
-    }
 }
 ready(() => {window.translatorToolInline = new translatorToolInline();
     console.log('translatorToolInline.js loaded')});
