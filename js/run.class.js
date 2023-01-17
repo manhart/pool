05b2233258e36b12f1d776650201fbafe21230ef
Alexander Manhart <alexander@manhart-it.de>
2023-01-18 00:45:29 +0100

    Moved javascripts to js

---
+++ js/run.class.js
@@ -0,0 +1,11 @@
+/*
+ * POOL
+ *
+ * Weblication.js created at 10.12.21, 12:00
+ *
+ * @author Alexander Manhart <alexander@manhart-it.de>
+ */
+
+$Weblication.run();
+
+console.debug('run.class.js executed');
