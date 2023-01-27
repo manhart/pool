f0a813e0da8634355968e0a15cf97128d3244e91
Alexander Manhart <alexander@manhart-it.de>
2023-01-27 13:27:24 +0100

    After refactoring, handling expired session (and access not allowed) via Ajax calls did not work anymore. Fixed problem by overwriting GUI_Module.parseAjaxResponse in GUI_G7Module with additional exception handling.

--- js/GUI_Module.class.js
+++ js/GUI_Module.class.js
@@ -70,14 +70,19 @@ class GUI_Module
         try {
             json = JSON.parse(text);
         }
-        catch(e) { // @todo only developers (box) should see the hole text/error
-            console.error('Unparsed text', text);
-            throw e;
+        catch(e) {
+            if(e instanceof SyntaxError) {
+                // ? should the framework handle this error
+            }
+            throw e
         }
         const { data, error, success } = response.status !== 204 ? json : { success: true };
 
         // trigger a new exception to capture later on request call site
-        if (!success) throw new Error(error.message);
+        if (!success) {
+            // notice: the pool responses with an error.type and error.message
+            throw new PoolAjaxResponseError(error.message, null, error.type);
+        }
         // Otherwise, simply resolve the received data
         return data;
     }
