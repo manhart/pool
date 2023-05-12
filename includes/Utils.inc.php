ff30ab24096e08f7e9fcbdd0eae786220a4eecec
Alexander Manhart <alexander@manhart-it.de>
2023-05-13 00:37:16 +0200

    Another Fix

--- includes/Utils.inc.php
+++ includes/Utils.inc.php
@@ -1394,7 +1394,7 @@ function makeRelativePathsFrom(?string $here, string $toThis, bool $normalize = 
 
     // Calculate the clientside relative path.
     $clientsideRelativePathComponents = count(explode($separator, $_SERVER['DOCUMENT_ROOT']));
-    $commonPathComponents = min($clientsideRelativePathComponents, $commonPathComponents);
+    $commonPathComponents = min($clientsideRelativePathComponents, $paths['clientside']['commonPathComponents']);
     $levels = str_repeat('..' . $separator, $clientsideRelativePathComponents - $paths['clientside']['commonPathComponents']) ?: '/';
     $clientsideRelativePath = $levels . implode($separator, array_slice($paths['clientside']['toThisParts'], $commonPathComponents));
 
