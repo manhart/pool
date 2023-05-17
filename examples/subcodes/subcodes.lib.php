32343f3cafef8fa9aa36b54a71eaa8302d60aaae
Alexander Manhart <alexander@manhart-it.de>
2023-05-17 16:16:33 +0200

    Removed pool examples

--- examples/subcodes/subcodes.lib.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alexander
- * Date: 12.02.2019
- * Time: 22:26
- */
--- examples/yourApp/javascripts/yourapp.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    console.debug('yourapp.js is ready');
-});
