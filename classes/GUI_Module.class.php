d8659527db896b335a87acb3b317dd9dc8711da5
Alexander Manhart <alexander@manhart-it.de>
2023-05-23 15:21:36 +0200

    Type conversion fixed

--- classes/GUI_Module.class.php
+++ classes/GUI_Module.class.php
@@ -675,19 +675,12 @@ class GUI_Module extends Module
                 $value = $this->Input->getVar($Parameter->getName(), ($Parameter->isOptional() ? $Parameter->getDefaultValue() : ''));
                 if (is_string($value)) {
                     if ($Parameter->hasType() && $Parameter->getType()->getName() != 'mixed') {
-                        switch ($Parameter->getType()->getName()) {
-                            case 'float':
-                                $value = (float)$value;
-                                break;
-
-                            case 'int':
-                                $value = (int)$value;
-                                break;
-
-                            case 'bool':
-                                $value = filter_var($value, FILTER_VALIDATE_BOOL);
-                                break;
-                        }
+                        $value = match ($Parameter->getType()->getName()) {
+                            'float' => (float)$value,
+                            'int' => (int)$value,
+                            'bool' => filter_var($value, FILTER_VALIDATE_BOOL),
+                            default => $value,
+                        };
                     } else {
                         if ($value === 'true' or $value === 'false') {
                             $value = $value === 'true';
