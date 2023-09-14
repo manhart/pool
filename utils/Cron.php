a08390a77a1ecc7bc9c3a6f0deb206e785cb3dcd
Alexander Manhart <alexander@manhart-it.de>
2023-09-14 14:48:48 +0200

    Initial import of namespace "pool\utils" starting with cron utilities/helpers.

---
+++ utils/Cron.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of POOL (PHP Object-Oriented Library)
+ *
+ * (c) Alexander Manhart <alexander@manhart-it.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace pool\utils;
+
+final class Cron
+{
+    /**
+     * Acquire a semaphore lock for a cron job
+     *
+     * @param string $file path to lock file
+     * @param string $projectId single-letter project ID
+     */
+    public static function acquireSemaphoreLock(string $file, string $projectId): bool
+    {
+        $semaphore = sem_get(ftok($file, $projectId));
+        if(sem_acquire($semaphore, true)) {
+            register_shutdown_function(static fn() => sem_release($semaphore));
+            return true;
+        }
+        return false;
+    }
+}
