671663cdabfea6f2223505f78d521201f36665f5
Alexander Manhart <alexander@manhart-it.de>
2023-10-04 09:49:59 +0200

    Only comments

--- classes/Core/Weblication.php
+++ classes/Core/Weblication.php
@@ -954,7 +954,7 @@ class Weblication extends Component
     }
 
     /**
-     * client-side relative path to the pool root directory
+     * Client-side relative path to the pool root directory
      *
      * @param string $subDir
      * @return string path from the application to the pool
@@ -965,7 +965,7 @@ class Weblication extends Component
     }
 
     /**
-     * server-side relative path to the pool root directory
+     * Server-side relative path to the pool root directory
      *
      * @param string $subDir
      * @return string path from the application to the pool
@@ -1027,7 +1027,7 @@ class Weblication extends Component
     }
 
     /**
-     * returns a DataInterface
+     * Returns a DataInterface
      *
      * @param string $interface_name
      * @return DataInterface|null Interface Objekt
@@ -1042,7 +1042,7 @@ class Weblication extends Component
      *
      * @return array Interface Objekte
      * @see DAO::createDAO()
-     **/
+     */
     public function getInterfaces(): array
     {
         return $this->interfaces;
@@ -1249,7 +1249,7 @@ class Weblication extends Component
     }
 
     /**
-     * returns the primary language based on the set locale
+     * Returns the primary language based on the set locale
      *
      * @return string language code
      */
@@ -1262,7 +1262,7 @@ class Weblication extends Component
     }
 
     /**
-     * returns cookie for this application
+     * Returns cookie for this application
      *
      * @return Cookie
      */
@@ -1285,7 +1285,7 @@ class Weblication extends Component
     }
 
     /**
-     * returns module that should be launched
+     * Returns module that should be launched
      *
      * @return string
      */
@@ -1295,7 +1295,7 @@ class Weblication extends Component
     }
 
     /**
-     * render application
+     * Render application
      *
      * @return void
      * @throws ModulNotFoundException
@@ -1315,7 +1315,7 @@ class Weblication extends Component
     }
 
     /**
-     * Erzeugt das erste GUI_Module in der Kette (Momentan wird hier der Seitentitel mit dem Projektnamen gefuellt).
+     * Creates the first GUI_Module in the chain (the page title is filled with the project name).
      *
      * @param string $className GUI_Module (Standard-Wert: GUI_CustomFrame)
      * @return Weblication
@@ -1345,7 +1345,7 @@ class Weblication extends Component
     }
 
     /**
-     * main logic of the front controller. compile main content.
+     * Main logic of the front controller. compile main content.
      */
     protected function prepareContent(): void
     {
@@ -1356,7 +1356,7 @@ class Weblication extends Component
     }
 
     /**
-     * return finished HTML content
+     * Return finished HTML content
      * Error handling wrapper around finalizeContent of the Main-GUI
      *
      * @return string website content
@@ -1404,7 +1404,7 @@ class Weblication extends Component
     }
 
     /**
-     * returns the current timezone
+     * Returns the current timezone
      *
      * @return string
      */
@@ -1425,7 +1425,7 @@ class Weblication extends Component
     }
 
     /**
-     * measure page speed and print it in the footer
+     * Measure page speed and print it in the footer
      *
      * @return void
      * @todo ajax requests?
