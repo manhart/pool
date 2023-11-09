83ced7c0f1a19f6749a4b9c4c34589cef4536ee4
Alexander Manhart <alexander@manhart-it.de>
2023-11-09 14:05:33 +0100

    Refactor DataType class to utilize constants and namespaces
    DataType class has been refactored to leverage PHP constants and namespace function calls. This change augments code readability, minimizes errors and brings more consistency with the language best practices.

--- classes/Core/Input/Filter/DataType.php
+++ classes/Core/Input/Filter/DataType.php
@@ -44,42 +44,42 @@ enum DataType
     {
         $alphaNum = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
         $filter = match ($dataType) {
-            DataType::INT => fn($value) => is_numeric($value) && (!is_string($value) || !str_contains($value, '.')) ? (int)$value :
+            self::INT => static fn($value) => \is_numeric($value) && (!\is_string($value) || !str_contains($value, '.')) ? (int)$value :
                 throw new \InvalidArgumentException('Value is not an integer'),
-            DataType::FLOAT => fn($value) => is_numeric($value) ? (float)$value : throw new \InvalidArgumentException('Value is not numeric.'),
-            DataType::ANY => fn($value) => $value,
-            DataType::TEXT => function($value) {
-                if($value !== '' && strcspn($value, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F") !==
-                    strlen($value)) {
+            self::FLOAT => static fn($value) => \is_numeric($value) ? (float)$value : throw new \InvalidArgumentException('Value is not numeric.'),
+            self::ANY => static fn($value) => $value,
+            self::TEXT => static function($value) {
+                if($value !== '' && \strcspn($value, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F") !==
+                    \strlen($value)) {
                     throw new \InvalidArgumentException('Value is not text.');
                 }
                 return (string)$value;
             },
-            DataType::BOOL => fn($value) => match ($value) {
+            self::BOOL => static fn($value) => match ($value) {
                 true, 1, '1', 'true', 'on', 'yes' => true,
                 false, 0, '0', 'false', 'off', 'no' => false,
                 default => throw new \InvalidArgumentException('Value is not boolean.')
             },
-            DataType::ARRAY => fn($value) => is_array($value) ? $value :
+            self::ARRAY => static fn($value) => \is_array($value) ? $value :
                 throw new \InvalidArgumentException('Value is not an array.'),
-            DataType::JSON => fn($value) => isValidJSON($value) ? $value :
+            self::JSON => static fn($value) => \isValidJSON($value) ? $value :
                 throw new \InvalidArgumentException('Value is not valid JSON.'),
-            DataType::EMAIL => fn($value) => is_string(filter_var($value, FILTER_VALIDATE_EMAIL)) ? $value :
+            self::EMAIL => static fn($value) => \is_string(\filter_var($value, \FILTER_VALIDATE_EMAIL)) ? $value :
                 throw new \InvalidArgumentException('Value is not a valid email address.'),
-            DataType::URL => fn($value) => is_string(filter_var($value, FILTER_VALIDATE_URL)) ? $value :
+            self::URL => static fn($value) => \is_string(\filter_var($value, \FILTER_VALIDATE_URL)) ? $value :
                 throw new \InvalidArgumentException('Value is not a valid URL.'),
-            DataType::NO_HTML => fn($value) => htmlentities($value, ENT_QUOTES | ENT_SUBSTITUTE),
-            DataType::ALPHANUMERIC => fn($value) => strcspn($value, $alphaNum) === 0 ? $value :
+            self::NO_HTML => static fn($value) => \htmlentities($value, \ENT_QUOTES | \ENT_SUBSTITUTE),
+            self::ALPHANUMERIC => static fn($value) => \strcspn($value, $alphaNum) === 0 ? $value :
                 throw new \InvalidArgumentException('Value is not alphanumeric.'),
-            DataType::ALPHANUMERIC_SPACE => fn($value) => strcspn($value, "$alphaNum ") === 0 ? $value :
+            self::ALPHANUMERIC_SPACE => static fn($value) => \strcspn($value, "$alphaNum ") === 0 ? $value :
                 throw new \InvalidArgumentException('Value is not alphanumeric with spaces.'),
-            DataType::ALPHANUMERIC_SPACE_PUNCTUATION => fn($value) => strcspn($value, "$alphaNum .,;:!?()-+/&") === 0 ? $value :
+            self::ALPHANUMERIC_SPACE_PUNCTUATION => static fn($value) => \strcspn($value, "$alphaNum .,;:!?()-+/&") === 0 ? $value :
                 throw new \InvalidArgumentException('Value is not alphanumeric with spaces and punctuation.'),
         };
         return match ($dataType) {
-            DataType::ANY => $filter,
-            default => function($value) use ($filter) {
-                return $value === chr(0) ? null : $filter($value);
+            self::ANY => $filter,
+            default => static function($value) use ($filter) {
+                return $value === \chr(0) ? null : $filter($value);
             }
         };
     }
