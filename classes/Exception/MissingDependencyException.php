9f044cf0252c848469ef76c82393f5dba9e1b59d
Alexander Manhart <alexander@manhart-it.de>
2023-11-22 12:11:55 +0100

    Added a new Exception "MissingDependencyException"

---
+++ classes/Exception/MissingDependencyException.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of POOL (PHP Object-Oriented Library)
+ *
+ * (c) Alexander Manhart <alexander@manhart-it.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace pool\classes\Exception;
+
+use RuntimeException as PhpRuntimeException;
+
+/**
+ * Thrown to indicate that the argument received is not valid.
+ */
+class MissingDependencyException extends PhpRuntimeException implements PoolExceptionInterface
+{
+}
