d001e60ff9e775fdfac410bd34b69074e3b50f5e
Alexander Manhart <alexander@manhart-it.de>
2023-11-29 13:07:56 +0100

    Geo Helper class with function to check a value for a coordinate according to WGS84

---
+++ utils/Geo.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of POOL (PHP Object-Oriented Library)
+ *
+ * (c) Alexander Manhart <alexander@manhart-it.de>
+ *
+ * For a list of contributors, please see the CONTRIBUTORS.md file
+ * @see https://github.com/manhart/pool/blob/master/CONTRIBUTORS.md
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, or visit the following link:
+ * @see https://github.com/manhart/pool/blob/master/LICENSE
+ *
+ * For more information about this project:
+ * @see https://github.com/manhart/pool
+ */
+
+namespace pool\utils;
+
+final class Geo
+{
+    public static function isValidWGS84(string $value): bool
+    {
+        return \preg_match('/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/', $value) === 1;
+    }
+}
