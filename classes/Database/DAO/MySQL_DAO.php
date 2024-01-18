0c9a27167094a77b858824d049c242b3c660c779
Alexander Manhart <alexander@manhart-it.de>
2024-01-18 08:37:09 +0100

    Significant optimization of the wrapSymbols method. Renamed wrapSymbols method to encloseColumnName.

--- classes/Database/DAO/MySQL_DAO.php
+++ classes/Database/DAO/MySQL_DAO.php
@@ -76,7 +76,7 @@ class MySQL_DAO extends DAO
         }
 
         $this->setColumns(...$this->columns);
-        $escapedColumns = array_map(fn($column) => "$this->quotedTable.$column", $this->columns);
+        $escapedColumns = array_map(fn($column) => "$this->quotedTable.$column", $this->escapedColumns);
         // Concatenate the columns into a single string
         $this->column_list = implode(', ', $escapedColumns);
     }
