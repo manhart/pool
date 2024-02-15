2efabc2e315f065d560caaa60cd9e7700266597f
Philipp Lehfeld <p.lehfeld@group-7.de>
2024-02-15 15:10:13 +0100

    add handling and documentation for cases where no record is found

--- classes/Database/DAO.php
+++ classes/Database/DAO.php
@@ -1059,13 +1059,13 @@ SQL;
      * Shorthand for fetching one or multiple values of a record
      * @param array|int|string $pk a unique identifier use an indexed array in the form [$column => $pk] to specify the primary key column
      * @param mixed ...$fields a list of columns to retrieve
-     * @return array|mixed the result, returns a list if multiple columns were queried
+     * @return array|mixed the result, returns a list if multiple columns were queried should there be no matching record returns null or an empty list respectively
      */
     public function fetchData(array|int|string $pk, ...$fields): mixed
     {
         if (is_array($pk)) /** @noinspection SuspiciousLoopInspection */ foreach (($pk) as $key => $pk) break;
         $record = $this->setColumns(...$fields)->get($pk, $key ?? null)->getRecord();
-        return count($fields) === 1 ? $record[$fields[0]] : array_values($record);
+        return count($fields) === 1 ? $record[$fields[0]] ?? null : array_values($record);
     }
 
     /**
