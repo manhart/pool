78628e52ed336fc804e84d4da363f5e654bce722
Alexander Manhart <alexander@manhart-it.de>
2024-09-18 17:07:11 +0200

    Removed outdated comments

--- js/array.js
+++ js/array.js
@@ -14,28 +14,6 @@
  * @see https://github.com/manhart/pool
  */
 
-
-
-/**
- * -= array.js =-
- *
- * Fuer alle Browser, die keine Array Funktionen shift, unshift, push und pop unterstuetzen (< IE 5.5 / Mac)
- * Plus erweiterte Funktionen, die man aus PHP kennt.
- *
- * $Log: array.js,v $
- * Revision 1.4  2004/09/23 14:08:02  manhart
- * Log Message included
- *
- *
- * @version $Id: array.js,v 1.4 2004/09/23 14:08:02 manhart Exp $
- * @version $Revision 1.0$
- * @version
- *
- * @since 2003-08-21
- * @author Alexander Manhart <alexander.manhart@freenet.de>
- * @link http://www.misterelsa.de
- */
-
 /**
  * Uniquely combines two arrays
  */
