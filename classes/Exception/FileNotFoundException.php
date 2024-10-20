414c90e7a24b2dae22f4b96e344ca0805abf8eb1
Alexander Manhart <alexander@manhart-it.de>
2024-10-20 22:09:07 +0200

    Add custom exceptions for file operations
    Introduce `FileNotFoundException` and `FileOperationException` for better error handling in file-related operations.

---
+++ classes/Exception/FileNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of POOL (PHP Object-Oriented Library)
+ *
+ * (c) Alexander Manhart <alexander@manhart-it.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace pool\classes\Exception;
+
+class FileNotFoundException extends MissingResourceException implements PoolExceptionInterface {}
