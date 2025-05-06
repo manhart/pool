393025e9f843026f5589e435a359d0d1e2f2def6
Leon Becher <l.becher@group-7.de>
2025-05-06 13:53:18 +0200

    use FileList as Template if no Template was specified

--- classes/GUI/GUI_Module.php
+++ classes/GUI/GUI_Module.php
@@ -717,6 +717,8 @@ class GUI_Module extends Module
         if (!array_key_exists('stdout', $templates) && in_array('stdout', $this->Template->getFiles(false))) {
             $templates['stdout'] = null;//patch for existing projects using loadFiles()
         }
+        $fileList = $this->Template->getFileList();
+        if (!$templates && $fileList) $templates = $fileList;
         foreach ($templates as $handle => $tplFile) {
             $content .= $this->Template->parse($handle)->getContent($handle);
         }
