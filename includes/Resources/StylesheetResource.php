c74bcc3e73547dbe60ee0f35d2d85b7b238119b1
Alexander Manhart <alexander@manhart-it.de>
2025-05-19 17:29:05 +0200

    Refactor: move resource interfaces to proper namespace location
    Relocated JavaScriptResource and StylesheetResource interfaces from dir\ResourceLoader.php into dedicated files under includes/Resources/.
    This cleanup ensures proper namespacing and aligns file structure with PSR-4 expectations.

---
+++ includes/Resources/StylesheetResource.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of POOL (PHP Object-Oriented Library)
+ *
+ * (c) Alexander Manhart <alexander@manhart-it.de>
+ *
+ * For a list of contributors, please see the CONTRIBUTORS.md file
+ * @see https://github.com/manhart/pool/blob/master/CONTRIBUTORS.md
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, or visit the following link:
+ * @see https://github.com/manhart/pool/blob/master/LICENSE
+ *
+ * For more information about this project:
+ * @see https://github.com/manhart/pool
+ */
+
+namespace pool\includes\Resources;
+
+interface StylesheetResource
+{
+    public const DEFAULT_FILE_EXT = '(css|scss)'; //todo scss less?
+}
